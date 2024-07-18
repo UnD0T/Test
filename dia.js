0a58173ff50e217b333a2cb525272abac36cc1b7
UnD0T <162162090+UnD0T@users.noreply.github.com>
2024-07-18 16:54:48 +0300

    Add files via upload

---
+++ dia.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  initialSlide: 0,
+  slidesPerView: 1,
+  spaceBetween: 25,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type: "bullets",
+  },
+});
